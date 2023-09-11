441a5925e3980beab6e164e606163c6a31cff13a
Edäin <mpizarrocuevas@gmail.com>
2023-09-11 12:30:04 +0200

    fix: sonarcloud codesmells

--- apps/backend/src/core/domain/UniqueEntityID.ts
+++ apps/backend/src/core/domain/UniqueEntityID.ts
@@ -3,6 +3,6 @@ import { Identifier } from './Identifier'
 
 export class UniqueEntityID extends Identifier<string | number>{
   constructor(id?: string | number) {
-    super(id ? id : crypto.randomUUID())
+    super(id || crypto.randomUUID())
   }
 }
